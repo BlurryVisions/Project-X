92a40b809ae29dfcff15d06d10550eda378cbe8e
Sourabh Pokhriyal <sourabhzrock@gmail.com>
2018-07-17 17:36:08 +0530

    535

--- Talhive/JS/header.js
+++ Talhive/JS/header.js
@@ -3,7 +3,7 @@ $(function() {
 
   $(window).scroll(function() {    
       var scroll = $(window).scrollTop();
-      if (scroll >= 500) {
+      if (scroll >= 600) {
           header.addClass("scrolled");
       } else {
           header.removeClass("scrolled");
