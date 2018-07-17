d2550214661eca3bbf4967513e49282411d39f6e
Sourabh Pokhriyal <sourabhzrock@gmail.com>
2018-07-17 05:56:32 +0530

    600

---
+++ Talhive/JS/header.js
@@ -0,0 +1,13 @@
+$(function() {
+  var header = $("header");
+
+  $(window).scroll(function() {    
+      var scroll = $(window).scrollTop();
+      if (scroll >= 500) {
+          header.addClass("scrolled");
+      } else {
+          header.removeClass("scrolled");
+      }
+  });
+
+});
