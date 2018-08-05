5149c71282928959cf0ab32f5c691d2c9f8d2446
Sourabh Pokhriyal <sourabhzrock@gmail.com>
2018-08-06 03:11:26 +0530

    PY

--- Talhive/Talhive/settings.py
+++ Talhive/Talhive/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Talhive.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Talhive',
+        'USER' : 'root',
+        'PASSWORD' : 'nOQgJIM8MdkodEqu',
+        'HOST' : 'localhost',
+        'PORT' : ''
     }
 }
 
