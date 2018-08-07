1c4217acb9ddc3a580179408146f762dd12f2375
Sourabh Pokhriyal <sourabhzrock@gmail.com>
2018-08-08 01:18:41 +0530

    Blog mockup done

--- Talhive/blog/models.py
+++ Talhive/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.TextField()
+    body = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
+    author = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
--- Talhive/blog/views.py
+++ Talhive/blog/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
 
+from .models import Blog
+
 def index(request):
-    return render(request, 'blog/index.html')
+    blogs = Blog.objects.all()[:10]
+
+    context = {
+        'title': 'Latest Posts',
+        'blog': blogs
+    }
+
+    return render(request, 'blog/blog.html', context)
+
+def details(request, id):
+    blog = Blog.objects.get(id=id)
+
+    context = {
+        'blog': blog
+    }
+
+    return render(request, 'blog/details.html', context)
